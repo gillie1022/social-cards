c5a88e02f4869c5e26bbec253dff950c88b837bc
gillie1022 <dave.gillie@icloud.com>
2020-07-07 15:50:44 -0400

    I think it works

--- cards/models.py
+++ cards/models.py
@@ -11,82 +11,51 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
 
-
 class Card(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='cards')
     outer_message = models.CharField(max_length=500, null=True, blank=True)
     inner_message = models.CharField(max_length=500, null=True, blank=True)
-    NO = 'None'
-    LC = 'Living Coral'
-    UV = 'Ultra Violet'
-    GR = 'Greenery'
-    RQ = 'Rose Quartz'
-    SE = 'Serenity'
-    MA = 'Marsala'
-    RO = 'Radiand Orchid'
-    EM = 'Emerald'
-    TT = 'Tangerine Tango'
-    HS = 'Honeysucle'
-    TQ = 'Turquoise'
-    MI = 'Mimosa'
-    BI = 'Blue Izis'
-    CP = 'Chili Pepper'
-    SD = 'Sand Dollar'
-    BT = 'Blue Turquoise'
-    TL = 'Tigerlily'
-    AS = 'Aqua Sky'
-    TR = 'True Red'
-    FR = 'Fuchsia Rose'
-    CB = 'Cerulean Blue'
-    COLOR_CHOICES = [
-        (NO, 'None'),
-        (LC, 'Living Coral'),
-        (UV, 'Ultra Violet'),
-        (GR, 'Greenery'),
-        (RQ, 'Rose Quartz'),
-        (SE, 'Serenity'),
-        (MA, 'Marsala'),
-        (RO, 'Radiand Orchid'),
-        (EM, 'Emerald'),
-        (TT, 'Tangerine Tango'),
-        (HS, 'Honeysucle'),
-        (TQ, 'Turquoise'),
-        (MI, 'Mimosa'),
-        (BI, 'Blue Izis'),
-        (CP, 'Chili Pepper'),
-        (SD, 'Sand Dollar'),
-        (BT, 'Blue Turquoise'),
-        (TL, 'Tigerlily'),
-        (AS, 'Aqua Sky'),
-        (TR, 'True Red'),
-        (FR, 'Fuchsia Rose'),
-        (CB, 'Cerulean Blue'),
-    ]
-    color = models.CharField(max_length=15, choices=COLOR_CHOICES, default=NO)
-    ML = 'Montserrat + Lora (Modern)'
-    PL = 'Prata + Lato (Elegant)'
-    AJ = 'Archivo Black + Judson (Emphasis)'
-    FONT_CHOICES = [
-        (ML, 'Montserrat + Lora (Modern)'),
-        (PL, 'Prata + Lato (Elegant)'),
-        (AJ, 'Archivo Black + Judson (Emphasis)'),
-    ]
-    font = models.CharField(max_length=33, choices=FONT_CHOICES, default=ML)
-    NONE = 'None'
-    SOLID = 'Solid'
-    INSET = 'Inset'
-    BORDER_CHOICES = [
-        (NONE, 'None'),
-        (SOLID, 'Solid'),
-        (INSET, 'Inset'),
-    ]
-    border = models.CharField(max_length=500, choices=BORDER_CHOICES, default=NONE)
+    
+    COLOR_CHOICES = (
+        ('None', 'None'),
+        ('Living Coral', 'Living Coral'),
+        ('Ultra Violet', 'Ultra Violet'),
+        ('Greenery', 'Greenery'),
+        ('Rose Quartz', 'Rose Quartz'),
+        ('Serenity', 'Serenity'),
+        ('Marsala', 'Marsala'),
+        ('Radiand Orchid', 'Radiand Orchid'),
+        ('Emerald', 'Emerald'),
+        ('Tangerine Tango', 'Tangerine Tango'),
+        ('Honeysucle', 'Honeysucle'),
+        ('Turquoise', 'Turquoise'),
+        ('Mimosa', 'Mimosa'),
+        ('Blue Izis', 'Blue Izis'),
+        ('Chili Pepper', 'Chili Pepper'),
+        ('Sand Dollar', 'Sand Dollar'),
+        ('Blue Turquoise', 'Blue Turquoise'),
+        ('Tigerlily', 'Tigerlily'),
+        ('Aqua Sky', 'Aqua Sky'),
+        ('True Red', 'True Red'),
+        ('Fuchsia Rose', 'Fuchsia Rose'),
+        ('Cerulean Blue', 'Cerulean Blue'),
+    )
+    color = models.CharField(max_length=15, choices=COLOR_CHOICES, default='None')
+    
+    FONT_CHOICES = (
+        ('Montserrat + Lora (Modern)', 'Montserrat + Lora (Modern)'),
+        ('Prata + Lato (Elegant)', 'Prata + Lato (Elegant)'),
+        ('Archivo Black + Judson (Emphasis)', 'Archivo Black + Judson (Emphasis)'),
+    )
+    font = models.CharField(max_length=33, choices=FONT_CHOICES, default='Montserrat + Lora (Modern)')
+    
+    BORDER_CHOICES = (
+        ('None', 'None'),
+        ('Solid', 'Solid'),
+        ('Inset', 'Inset'),
+    )
+    border = models.CharField(max_length=500, choices=BORDER_CHOICES, default='None')
     posted_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-posted_at']
-
-def get_cards_from_those_user_follows(queryset, user):
-    if user.is_authenticated:
-        cards = queryset.filter(Q(author in user.follows.all()))
-    return cards
--- cards/views.py
+++ cards/views.py
@@ -2,31 +2,35 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets, permissions, views
 from users.models import User 
 from cards.models import Card
-from cards.serializers import UserSerializer, CardSerializer, UserFollowsSerializer
+from cards.serializers import UserSerializer, CardSerializer, UserFollowsSerializer, AuthorSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.method in permissions.SAFE_METHODS or (request.user and request.user.is_authenticated))
+
+    def has_object_permission(self, request, view, obj):
+        return bool(request.method in permissions.SAFE_METHODS or (request.user and request.user.is_authenticated and obj.author == request.user))
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
-
 class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
     serializer_class = CardSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return Card.objects.filter(author__fans=self.request.user)
+    permission_classes = [IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    @action(detail=False, methods=['GET', 'POST'], permission_classes=[permissions.IsAuthenticated])
-    def me(self, request):
+    @action(detail=False, methods=['GET'], permission_classes=[permissions.IsAuthenticated])
+    def my_cards(self, request):
         cards = request.user.cards.all()
         
         page = self.paginate_queryset(cards)
@@ -37,9 +41,9 @@ class CardViewSet(viewsets.ModelViewSet):
         serializer = CardSerializer(cards, many=True, context={'request': request})
         return Response(serializer.data)
     
-    @action(detail=False, methods=['GET', 'POST'], permission_classes=[permissions.IsAuthenticated])
-    def all(self, request):
-        cards = Card.objects.all()
+    @action(detail=False, methods=['GET'], permission_classes=[permissions.IsAuthenticated])
+    def followed_users_cards(self, request):
+        cards = Card.objects.filter(author__fans=self.request.user)
         page = self.paginate_queryset(cards)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
@@ -47,18 +51,22 @@ class CardViewSet(viewsets.ModelViewSet):
         serializer = CardSerializer(cards, many=True, context={'request': request})
         return Response(serializer.data)
 
+
 class UserFollowsView(views.APIView):
     permission_classes=[permissions.IsAuthenticated]
 
     def get(self, request, format=None):
-        usernames = [user.username for user in request.user.followed_users.all()]
-        return Response(usernames)
+        user_follows = request.user.followed_users.all()
+        serializer = AuthorSerializer(user_follows, many=True, context={'request': request})
+        return Response(serializer.data)
         
     def post(self, request, format=None):
         username = request.data["user"]
         user = User.objects.get(username=username)
         request.user.followed_users.add(user)
-        return Response(request.data)
+        serializer = AuthorSerializer(user, context={"request": request})
+        return Response(serializer.data)
+
 
 class UnfollowView(views.APIView):
     def get(self, request, followed_user_username, format=None):
